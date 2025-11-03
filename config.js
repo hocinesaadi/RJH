d341764b64867d0c40382f5dc559994564cd7891
saadi <34628341+hocinesaadi@users.noreply.github.com>
2025-11-03 11:24:49 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+const CONFIG = {
+    titleWeb: "Pour Rokia — de Hocine",
+    introTitle: 'Rokia, mon amour',
+    introDesc: `Aujourd'hui, en novembre, cela fait déjà 1 an
+    que notre belle histoire a commencé.
+    Moi, Hocine, je te promets des fleurs, des rires
+    et d'être avec toi jusqu'à la fin de cette vie. 💖`,
+    btnIntro: 'Continuer 💕',
+    title: 'Rokia, veux-tu être pour toujours avec moi ? 🥰',
+    desc: "En ce mois de novembre, je célèbre 1 an avec ma belle Rokia.",
+    btnYes: 'Oui, je t\'aime <33',
+    btnNo: 'Oh non 😅',
+    question: 'Dis-moi pourquoi tu m\'aimes, Rokia 🥰',
+    btnReply: 'Envoyer à Hocine <3',
+    reply: "Parce que je t'aime pour toujours, mon coeur 💞",
+    mess: 'Je le savais ! Je t\'aime encore plus ❤️',
+    messDesc: "Pour toujours ensemble, jusqu'à la fin de cette vie.",
+    btnAccept: 'D\'accord, mon coeur 💞',
+    messLink: '',
+    startDate: '2024-11-01',
+    loveMessage: "Rokia, chaque jour à tes côtés est un cadeau. Je t'aime au-delà des mots, aujourd'hui, demain et pour toujours. — Hocine"
+}
